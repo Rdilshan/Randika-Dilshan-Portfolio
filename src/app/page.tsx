b9c973f3427d6f77e3f55d524400692c7c5260a0
Randika Dilshan <rdilshan077788@gmail.com>
2024-10-08 19:40:40 +0530

    font style add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,9 +54,9 @@ export default function Home() {
 
       <div id="section2" className="h-screen relative align-middle flex flex-col justify-center bg-black mt-[100vh]" >
 
-        <div className=" bg-zinc-900 rounded-sm p-10 md:flex sm:flex px-10">
-          <p className="text-2xl">Hi, I’m Randika,a designer and front-end developer based in Wales.</p>
-          <div className="text-2xl mt-10 md:mt-0">With over a decade of experience in multiple disciplines, I work with companies to create intuitive and engaging digital experiences.</div>
+        <div className=" bg-zinc-900 px-10 rounded-sm p-10 md:flex sm:flex md:px-32 ">
+          <p className="text-4xl md:text-5xl font-League font-thin tracking-wider leading-tight">Hi, Im Randika,a designer and front-end developer based in Wales.</p>
+          <p className="text-3xl font-League tracking-wider leading-tight mt-10 md:mt-0">With over a decade of experience in multiple disciplines, I work with companies to create intuitive and engaging digital experiences.</p>
         </div>
 
 
