aad6b3bbc9e3ff93c90b53df6488d73aeedf6c31
Randika Dilshan <rdilshan077788@gmail.com>
2024-10-08 19:40:33 +0530

    z index add

--- src/app/(components)/Footbar.jsx
+++ src/app/(components)/Footbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footbar() {
     return (
         <>
-            <div className="fixed bottom-5 w-full flex justify-center">
+            <div className="fixed bottom-5 w-full flex justify-center z-50">
                 <div className="flex justify-center items-center p-1 gap-5 bg-zinc-800 w-1/2 h-7 rounded-2xl">
                     <p className="text-xs bg-black p-2 rounded-3xl tracking-widest">Home</p>
                     <p className="text-xs tracking-widest">About</p>
