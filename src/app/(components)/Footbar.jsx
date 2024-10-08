8eb5b5ccc5adae0721b69d729a05fd51b3759f5a
Randika Dilshan <rdilshan077788@gmail.com>
2024-10-08 20:13:12 +0530

    after section1 showing footbar

--- src/app/(components)/Footbar.jsx
+++ src/app/(components)/Footbar.jsx
@@ -1,16 +1,42 @@
-import React from 'react'
+'use client';
+
+import React, { useEffect, useState } from 'react';
 
 function Footbar() {
+
+    const [showTypewriter, setShowTypewriter] = useState(false);
+
+    const handleScroll = () => {
+      if (window.scrollY > window.innerHeight) {
+        setShowTypewriter(true);
+      } else {
+        setShowTypewriter(false);
+      }
+    };
+  
+
+    useEffect(() => {
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+
     return (
         <>
-            <div className="fixed bottom-5 w-full flex justify-center z-50">
-                <div className="flex justify-center items-center p-1 gap-5 bg-zinc-800 w-1/2 h-7 rounded-2xl">
-                    <p className="text-xs bg-black p-2 rounded-3xl tracking-widest">Home</p>
-                    <p className="text-xs tracking-widest">About</p>
-                    <p className="text-xs tracking-widest">Contact</p>
-                </div>
-            </div>
-
+       {showTypewriter && (
+        <div
+          className={`fixed bottom-5 w-full flex justify-center z-50 transition-all duration-500 transform ${
+            showTypewriter ? 'opacity-100 translate-x-4' : 'opacity-0 translate-y-0'
+          }`}
+        >
+          <div className="flex justify-center items-center p-1 gap-5 bg-zinc-800 w-1/2 h-7 rounded-2xl">
+            <p className="text-xs bg-black p-2 rounded-3xl tracking-widest">Home</p>
+            <p className="text-xs tracking-widest">About</p>
+            <p className="text-xs tracking-widest">Contact</p>
+          </div>
+        </div>
+      )}
         </>
     )
 }
