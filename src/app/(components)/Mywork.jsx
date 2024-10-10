701798fb8f6d4323d3047fa476a03b5d1132dd25
Randika Dilshan <rdilshan077788@gmail.com>
2024-10-10 07:14:06 +0530

    create compoment mywork 🏒

---
+++ src/app/(components)/Mywork.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Mywork() {
+    return (
+        <>
+            <div id="section3" className="h-screen relative flex flex-col items-center bg-black py-10 " >
+
+                <h1 className="text-5xl font-League font-thin tracking-wider leading-tight">LATEST WORK</h1>
+                <div className="bg-red-500 w-full h-3/4 flex justify-center">
+                    <div className="bg-cyan-300 w-3/4">
+
+                        <div className="bg-zinc-900">
+                            <span>a decade of experience</span>
+                            <div className="w-10">
+                                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="m0 0h24v24h-24z" fill="#fff" opacity="0" transform="matrix(-1 0 0 -1 24 24)" />
+                                    <path
+                                        d="m18 7.05a1 1 0 0 0 -1-1l-8-.05a1 1 0 0 0 0 2h5.56l-8.27 8.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l8.29-8.29v5.58a1 1 0 0 0 1 1 1 1 0 0 0 1-1z"
+                                        fill="#ffffff" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div>body</div>
+
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Mywork
