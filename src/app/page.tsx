81ad5ddd7cfa195cc335cdfc69466a2ac647838f
Randika Dilshan <rdilshan077788@gmail.com>
2024-10-11 06:34:51 +0530

    lask work box finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -68,25 +68,30 @@ export default function Home() {
 
       <div id="section3" className="h-screen relative flex flex-col items-center bg-black py-10" >
 
-        <h1 className="text-5xl font-League font-thin tracking-wider leading-tight">LATEST WORK</h1>
-        <div className="bg-red-500 w-full h-3/4 flex justify-center">
-          <div className="bg-cyan-300 w-3/4">
+        <h1 className="text-5xl font-League font-thin tracking-wider leading-tight ">LATEST WORK</h1>
 
-            <div className="bg-zinc-900">
-              <span>a decade of experience</span>
-              <div className="w-10">
+        <div className=" w-full h-3/4 flex justify-center">
+          <div className="bg-white w-3/4 rounded-b-lg">
+            <div className="bg-zinc-900 flex justify-between rounded-b-lg">
+              <span className="flex items-center ml-5">a decade of experience</span>
+              <div className="w-5 md:w-8 border border-white rounded-md m-5">
+
+
                 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path d="m0 0h24v24h-24z" fill="#fff" opacity="0" transform="matrix(-1 0 0 -1 24 24)" />
                   <path
                     d="m18 7.05a1 1 0 0 0 -1-1l-8-.05a1 1 0 0 0 0 2h5.56l-8.27 8.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l8.29-8.29v5.58a1 1 0 0 0 1 1 1 1 0 0 0 1-1z"
                     fill="#ffffff" />
                 </svg>
+
+
               </div>
             </div>
-            <div>body</div>
-
+            <h1 className="flex justify-center items-center text-slate-950">Image 1</h1>
           </div>
         </div>
+
+        
 
       </div>
 
