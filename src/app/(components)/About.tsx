3b6ec2aa55b9a00a9656e3edb1a4d63ad620f15f
Randika Dilshan <rdilshan077788@gmail.com>
2024-10-12 05:14:06 +0530

    Abot section text add

--- src/app/(components)/About.tsx
+++ src/app/(components)/About.tsx
@@ -6,8 +6,8 @@ function About() {
             <div id="section2" className="h-screen relative align-middle flex flex-col justify-center bg-black mt-[100vh]" >
 
                 <div className=" bg-zinc-900 px-10 rounded-sm p-10 md:flex sm:flex md:px-32 ">
-                    <p className="text-4xl md:text-5xl font-League font-thin tracking-wider leading-tight">Hi, Im Randika,a designer and front-end developer based in Wales.</p>
-                    <p className="text-3xl font-League tracking-wider leading-tight mt-10 md:mt-0">With over a decade of experience in multiple disciplines, I work with companies to create intuitive and engaging digital experiences.</p>
+                    <p className="text-4xl md:text-5xl font-League font-thin tracking-wider leading-tight text-left flex-1">Hi, I'm Randika, an undergraduate software engineer.</p>
+                    <p className="text-2xl md:text-3xl font-League tracking-wider leading-tight mt-10 md:mt-0 text-justify flex-1">I'm currently pursuing a degree in Software Engineering with a strong passion for programming languages and software development. My journey in tech has fueled my enthusiasm for solving complex problems and creating innovative solutions.</p>
                 </div>
 
             </div>
