c627f478d51161841b55867f394013f97d1ae482
Randika Dilshan <rdilshan077788@gmail.com>
2024-10-14 08:29:44 +0530

    add the preloading for pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-
-
+'use client';
+import { useState, useEffect } from "react";
 import Footbar from "./(components)/Footbar";
 import About from "./(components)/About";
 import Contact from "./(components)/Contact";
@@ -7,21 +7,39 @@ import Lastwork from "./(components)/Lastwork";
 import Mainpage from "./(components)/Mainpage";
 import Preloading from "./(components)/Preloading";
 
-
 export default function Home() {
+  const [showPreloading, setShowPreloading] = useState(true);
+  const [fadeOut, setFadeOut] = useState(false); 
+  const [fadeIn, setFadeIn] = useState(false); 
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFadeOut(true); 
+      setTimeout(() => {
+        setShowPreloading(false); 
+      }, 1000); 
+    }, 2200);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
 
+      {showPreloading && (
+        <div className={fadeOut ? "fade-out" : ""}>
+          <Preloading />
+        </div>
+      )}
 
-      {/* <Footbar /> */}
-      <Preloading/>
-      
-      {/* <Mainpage />
-      <About />
-      <Lastwork />
-      <Contact /> */}
-
-
+      {!showPreloading && (
+        <>
+          <Mainpage />
+          <About />
+          <Lastwork />
+          <Contact />
+        </>
+      )}
     </>
   );
 }
