2fc4ad525c859dd01c9e5665c82b9c4dba203dc2
Randika Dilshan <rdilshan077788@gmail.com>
2024-10-14 12:36:40 +0530

    images add on my lask work 🥼

--- src/app/(components)/Lastwork.tsx
+++ src/app/(components)/Lastwork.tsx
@@ -1,30 +1,39 @@
-import React from 'react'
-
+import Image from 'next/image';
+import Link from 'next/link';
+import MedExplorer from "../../../public/MedExplorer.gif";
+import Whatsappwebloacker from "../../../public/webloacker.gif";
+import Handwriting from "../../../public/Handwritten Recognition System.gif";
+import vehiclewebsites from "../../../public/vehicle sale websites.gif";
+import Npmimg from "../../../public/npm.gif"
 function Lastwork() {
     return (
         <>
             <div id="section3" className="h-screen relative flex flex-col items-center bg-black py-10" >
 
-                <h1 className="text-5xl font-League font-thin tracking-wider leading-tight ">LATEST WORK</h1>
+                <h1 className="text-5xl font-League font-thin tracking-wider leading-tight mb-10">LATEST WORK</h1>
 
-                <div className=" w-full h-3/4 flex justify-center">
-                    <div className="bg-white w-3/4 rounded-b-lg">
+                <div className=" w-full md:h-3/4 flex justify-center">
+                    <div className="bg-zinc-900 w-[60vh] md:w-[95vh]  rounded-b-lg">
+
                         <div className="bg-zinc-900 flex justify-between rounded-b-lg">
-                            <span className="flex items-center ml-5">a decade of experience</span>
-                            <div className="w-5 md:w-8 border border-white rounded-md m-5">
+                            <span className="flex items-center ml-5 text-[2vh] md:text-[3vh]">MedExplorer App</span>
+                            <Link href={"https://www.linkedin.com/posts/randikadilshanmanamendra_healthcare-machinelearning-appdevelopment-activity-7245106721256341504-ZaWg?utm_source=share&utm_medium=member_desktop"}>
+                                <div className="w-5 md:w-8 border border-white rounded-md m-5">
 
 
-                                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="m0 0h24v24h-24z" fill="#fff" opacity="0" transform="matrix(-1 0 0 -1 24 24)" />
-                                    <path
-                                        d="m18 7.05a1 1 0 0 0 -1-1l-8-.05a1 1 0 0 0 0 2h5.56l-8.27 8.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l8.29-8.29v5.58a1 1 0 0 0 1 1 1 1 0 0 0 1-1z"
-                                        fill="#ffffff" />
-                                </svg>
+                                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="m0 0h24v24h-24z" fill="#fff" opacity="0" transform="matrix(-1 0 0 -1 24 24)" />
+                                        <path
+                                            d="m18 7.05a1 1 0 0 0 -1-1l-8-.05a1 1 0 0 0 0 2h5.56l-8.27 8.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l8.29-8.29v5.58a1 1 0 0 0 1 1 1 1 0 0 0 1-1z"
+                                            fill="#ffffff" />
+                                    </svg>
 
 
-                            </div>
+                                </div>
+                            </Link>
                         </div>
-                        <h1 className="flex justify-center items-center text-slate-950">Image 1</h1>
+
+                        <Image src={MedExplorer} alt='image' className="object-cover h-auto" />
                     </div>
                 </div>
 
@@ -33,24 +42,29 @@ function Lastwork() {
             <div id="imglastwork" className="h-screen relative flex flex-col items-center " >
 
 
-                <div className=" w-full h-3/4 flex justify-center mt-10">
-                    <div className="bg-white w-3/4 rounded-b-lg">
+                <div className=" w-full md:h-3/4 flex justify-center">
+                    <div className="bg-zinc-900 w-[60vh] md:w-[95vh]  rounded-b-lg">
+
                         <div className="bg-zinc-900 flex justify-between rounded-b-lg">
-                            <span className="flex items-center ml-5">a decade of experience</span>
-                            <div className="w-5 md:w-8 border border-white rounded-md m-5">
+                            <span className="flex items-center ml-5 text-[2vh] md:text-[3vh]">WhatsLock Web</span>
+                            <Link href={"https://chromewebstore.google.com/detail/whatsapp-web-locker/nhgekppbkjkhjalncjhbkcaiblljkdna?authuser=1&hl=en-GB"}>
+                                <div className="w-5 md:w-8 border border-white rounded-md m-5">
 
 
-                                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="m0 0h24v24h-24z" fill="#fff" opacity="0" transform="matrix(-1 0 0 -1 24 24)" />
-                                    <path
-                                        d="m18 7.05a1 1 0 0 0 -1-1l-8-.05a1 1 0 0 0 0 2h5.56l-8.27 8.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l8.29-8.29v5.58a1 1 0 0 0 1 1 1 1 0 0 0 1-1z"
-                                        fill="#ffffff" />
-                                </svg>
+                                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="m0 0h24v24h-24z" fill="#fff" opacity="0" transform="matrix(-1 0 0 -1 24 24)" />
+                                        <path
+                                            d="m18 7.05a1 1 0 0 0 -1-1l-8-.05a1 1 0 0 0 0 2h5.56l-8.27 8.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l8.29-8.29v5.58a1 1 0 0 0 1 1 1 1 0 0 0 1-1z"
+                                            fill="#ffffff" />
+                                    </svg>
 
 
-                            </div>
+                                </div>
+                            </Link>
                         </div>
-                        <h1 className="flex justify-center items-center text-slate-950">Image 2</h1>
+
+
+                        <Image src={Whatsappwebloacker} alt='image' className="object-cover h-auto" />
                     </div>
                 </div>
 
@@ -59,24 +73,28 @@ function Lastwork() {
             <div id="imglastwork" className="h-screen relative flex flex-col items-center " >
 
 
-                <div className=" w-full h-3/4 flex justify-center mt-10">
-                    <div className="bg-white w-3/4 rounded-b-lg">
+                <div className=" w-full md:h-3/4 flex justify-center">
+                    <div className="bg-zinc-900 w-[60vh] md:w-[95vh]  rounded-b-lg">
+
                         <div className="bg-zinc-900 flex justify-between rounded-b-lg">
-                            <span className="flex items-center ml-5">a decade of experience</span>
-                            <div className="w-5 md:w-8 border border-white rounded-md m-5">
+                            <span className="flex items-center ml-5 text-[2vh] md:text-[3vh]">Handwritten Recognition System</span>
+                            <Link href={"https://www.linkedin.com/posts/randikadilshanmanamendra_machinelearning-ai-computervision-activity-7215632413656301568-mU7z?utm_source=share&utm_medium=member_desktop"}>
+                                <div className="w-5 md:w-8 border border-white rounded-md m-5">
 
 
-                                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="m0 0h24v24h-24z" fill="#fff" opacity="0" transform="matrix(-1 0 0 -1 24 24)" />
-                                    <path
-                                        d="m18 7.05a1 1 0 0 0 -1-1l-8-.05a1 1 0 0 0 0 2h5.56l-8.27 8.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l8.29-8.29v5.58a1 1 0 0 0 1 1 1 1 0 0 0 1-1z"
-                                        fill="#ffffff" />
-                                </svg>
+                                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="m0 0h24v24h-24z" fill="#fff" opacity="0" transform="matrix(-1 0 0 -1 24 24)" />
+                                        <path
+                                            d="m18 7.05a1 1 0 0 0 -1-1l-8-.05a1 1 0 0 0 0 2h5.56l-8.27 8.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l8.29-8.29v5.58a1 1 0 0 0 1 1 1 1 0 0 0 1-1z"
+                                            fill="#ffffff" />
+                                    </svg>
 
 
-                            </div>
+                                </div>
+                            </Link>
                         </div>
-                        <h1 className="flex justify-center items-center text-slate-950">Image 2</h1>
+
+                        <Image src={Handwriting} alt='image' className="object-cover h-auto" />
                     </div>
                 </div>
 
@@ -84,24 +102,29 @@ function Lastwork() {
             <div id="imglastwork" className="h-screen relative flex flex-col items-center " >
 
 
-                <div className=" w-full h-3/4 flex justify-center mt-10">
-                    <div className="bg-white w-3/4 rounded-b-lg">
+                <div className=" w-full md:h-3/4 flex justify-center">
+                    <div className="bg-zinc-900 w-[60vh] md:w-[95vh]  rounded-b-lg">
+
                         <div className="bg-zinc-900 flex justify-between rounded-b-lg">
-                            <span className="flex items-center ml-5">a decade of experience</span>
-                            <div className="w-5 md:w-8 border border-white rounded-md m-5">
+                            <span className="flex items-center ml-5 text-[2vh] md:text-[3vh]">
+                                Discovering Sri Lanka's Best Sales.</span>
+                            <Link href={"https://www.linkedin.com/posts/randikadilshanmanamendra_im-happy-to-share-this-vehical-buysearch-activity-7206147998676905984-rRE1?utm_source=share&utm_medium=member_desktop"}>
+                                <div className="w-5 md:w-8 border border-white rounded-md m-5">
 
 
-                                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="m0 0h24v24h-24z" fill="#fff" opacity="0" transform="matrix(-1 0 0 -1 24 24)" />
-                                    <path
-                                        d="m18 7.05a1 1 0 0 0 -1-1l-8-.05a1 1 0 0 0 0 2h5.56l-8.27 8.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l8.29-8.29v5.58a1 1 0 0 0 1 1 1 1 0 0 0 1-1z"
-                                        fill="#ffffff" />
-                                </svg>
+                                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="m0 0h24v24h-24z" fill="#fff" opacity="0" transform="matrix(-1 0 0 -1 24 24)" />
+                                        <path
+                                            d="m18 7.05a1 1 0 0 0 -1-1l-8-.05a1 1 0 0 0 0 2h5.56l-8.27 8.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l8.29-8.29v5.58a1 1 0 0 0 1 1 1 1 0 0 0 1-1z"
+                                            fill="#ffffff" />
+                                    </svg>
 
 
-                            </div>
+                                </div>
+                            </Link>
                         </div>
-                        <h1 className="flex justify-center items-center text-slate-950">Image 4</h1>
+                        <Image src={vehiclewebsites} alt='image' className="object-cover h-auto" />
+
                     </div>
                 </div>
 
@@ -109,24 +132,28 @@ function Lastwork() {
             <div id="imglastworklast" className="h-screen relative flex flex-col items-center " >
 
 
-                <div className=" w-full h-3/4 flex justify-center mt-10">
-                    <div className="bg-white w-3/4 rounded-b-lg">
+                <div className=" w-full md:h-3/4 flex justify-center">
+                    <div className="bg-zinc-900 w-[60vh] md:w-[95vh]  rounded-b-lg">
+
                         <div className="bg-zinc-900 flex justify-between rounded-b-lg">
-                            <span className="flex items-center ml-5">a decade of experience</span>
-                            <div className="w-5 md:w-8 border border-white rounded-md m-5">
+                            <span className="flex items-center ml-5 text-[2vh] md:text-[3vh]">
+                            Sri Lanka Administrative Divisions Package</span>
+                            <Link href={"https://www.npmjs.com/package/@rdilshan/gn-division"}>
+                                <div className="w-5 md:w-8 border border-white rounded-md m-5">
 
 
-                                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="m0 0h24v24h-24z" fill="#fff" opacity="0" transform="matrix(-1 0 0 -1 24 24)" />
-                                    <path
-                                        d="m18 7.05a1 1 0 0 0 -1-1l-8-.05a1 1 0 0 0 0 2h5.56l-8.27 8.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l8.29-8.29v5.58a1 1 0 0 0 1 1 1 1 0 0 0 1-1z"
-                                        fill="#ffffff" />
-                                </svg>
+                                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="m0 0h24v24h-24z" fill="#fff" opacity="0" transform="matrix(-1 0 0 -1 24 24)" />
+                                        <path
+                                            d="m18 7.05a1 1 0 0 0 -1-1l-8-.05a1 1 0 0 0 0 2h5.56l-8.27 8.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l8.29-8.29v5.58a1 1 0 0 0 1 1 1 1 0 0 0 1-1z"
+                                            fill="#ffffff" />
+                                    </svg>
 
 
-                            </div>
+                                </div>
+                            </Link>
                         </div>
-                        <h1 className="flex justify-center items-center text-slate-950">Image 5</h1>
+                        <Image src={Npmimg} alt='image' className="object-cover h-auto" />
                     </div>
                 </div>
 
