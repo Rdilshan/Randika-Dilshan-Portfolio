d9ae4f70f24863d0e586d13134faf0bf559b0c7e
Randika Dilshan <rdilshan077788@gmail.com>
2024-10-14 08:14:39 +0530

    preloading screen done 🎈

--- src/app/(components)/Preloading.jsx
+++ src/app/(components)/Preloading.jsx
@@ -1,20 +1,44 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 function preloading() {
-  const showmsg = () => {
-    console.log("click")
-  }
+
+  const [value, setvalue] = useState(100);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setvalue((prevValue) => {
+        if (prevValue > 0) {
+          console.log("Running");
+          return prevValue - 1;
+        } else {
+          clearInterval(timer); 
+          return 0;
+        }
+      });
+    }, 20);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <>
-      <div className="h-screen flex justify-center items-center" onClick={showmsg}>
-        <p className="font-bangers text-2xl md:text-4xl tracking-widest">Randika</p>
-        
-        after few secand this part show fristly this one hide
-        <div className="mx-4 md:mx-6  flex justify-center w-16 h-16 border border-white rounded-full">
-          <span className="text-4xl">.</span>
+      <div className="h-screen flex justify-center items-center" >
+        <p className="font-bangers text-2xl md:text-4xl tracking-widest animate-fadeinleft">Randika</p>
+
+
+
+        <div className="relative size-12 mx-4 md-mx-6 flex justify-center">
+          <span className="font-bangers absolute mt-2 animate-dropin">.</span>
+
+          <svg className="size-full -rotate-90" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="18" cy="18" r="16" fill="none" className="stroke-current text-blue-600 dark:text-blue-500" strokeWidth="1" strokeDasharray="100" strokeDashoffset={value} strokeLinecap="round"></circle>
+          </svg>
+
         </div>
-        
-        <div className="font-bangers text-2xl md:text-4xl tracking-widest">Dilshan</div>
+
+        <div className="font-bangers text-2xl md:text-4xl tracking-widest animate-fadeinright">Dilshan</div>
       </div>
     </>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,27 +9,50 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        bangers:["Bangers", "system-ui"],
-        League:["League Gothic", "sans-serif"]
+        bangers: ["Bangers", "system-ui"],
+        League: ["League Gothic", "sans-serif"],
       },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
       keyframes: {
-        wave: {
-          '0%': { transform: 'rotate(0.0deg)' },
-          '10%': { transform: 'rotate(14deg)' },
-          '20%': { transform: 'rotate(-8deg)' },
-          '30%': { transform: 'rotate(14deg)' },
-          '40%': { transform: 'rotate(-4deg)' },
-          '50%': { transform: 'rotate(10.0deg)' },
-          '60%': { transform: 'rotate(0.0deg)' },
-          '100%': { transform: 'rotate(0.0deg)' },
+        "fade-in-left": {
+          "0%": {
+            opacity: "0",
+            transform: "translate3d(-100%, 0, 0)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate3d(0, 0, 0)",
+          },
+        },
+        "fade-in-right": {
+          "0%": {
+            opacity: "0",
+            transform: "translate3d(100%, 0, 0)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate3d(0, 0, 0)",
+          },
+        },
+        "drop-in": {
+          "0%": {
+            opacity: "0",
+            transform: "scale(0)",
+            animationTimingFunction: "cubic-bezier(0.34, 1.61, 0.7, 1)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "scale(1)",
+          },
         },
       },
       animation: {
-        'waving-hand': 'wave 2s linear infinite',
+        fadeinleft: "fade-in-left 1s ease-in-out 0.25s 1",
+        fadeinright: "fade-in-right 1s ease-in-out 0.25s 1",
+        dropin: "drop-in 0.5s ease-in-out 0.25s 1",
       },
     },
   },
